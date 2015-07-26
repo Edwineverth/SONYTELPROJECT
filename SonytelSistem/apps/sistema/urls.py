2610dd46e053ccfa1510f62902a7d71a224fdd9a
Edwineverth <edwineverth94@gmail.com>
2015-07-26 17:10:49 -0500

    nueva actualizacion

--- SonytelSistem/apps/sistema/urls.py
+++ SonytelSistem/apps/sistema/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from .views import index,registrarCliente,reporteCliente,listarCliente
+from .views import index,registrarCliente,reporteCliente,listarCliente,buscarCliente,buscadorAjaxCliente,busqueda,editar,eliminar
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'SonytelSistem.views.home', name='home'),
@@ -8,5 +8,10 @@ urlpatterns = patterns('',
     url(r'^registrar/$', registrarCliente.as_view(),name='registrar_cliente'),
     url(r'^reporte/$', reporteCliente.as_view(),name='reporte_cliente'),
     url(r'^listado/$', listarCliente.as_view(),name='listar_cliente'),
+    url(r'^buscar/$', buscarCliente.as_view(),name='buscar_cliente'),
+    url(r'^buscador/$', buscadorAjaxCliente.as_view(),name='buscar_clienteajax'),
+    url(r'^busqueda/$', busqueda.as_view(),name='busqueda'),
+    url(r'^editar/(?P<pk>[\d]+)$', editar.as_view(),name='editar'),
+    url(r'^eliminar/(?P<pk>[\d]+)$', eliminar.as_view(),name='eliminar'),
     
 )
--- SonytelSistem/apps/sistema/views.py
+++ SonytelSistem/apps/sistema/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from .models import Clientes
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView,CreateView,ListView,UpdateView,DeleteView
+from django.http import HttpResponse
 # Create your views here.
 class index(TemplateView):
 	template_name='inicio/index.html'
+#CREAR
 class registrarCliente(CreateView):
 	template_name='cliente/registrarcliente.html'
 	model=Clientes
@@ -17,3 +19,34 @@ class listarCliente(ListView):
 	template_name='cliente/reportarcliente.html'
 	context_object_name='clientes'
 	model=Clientes
+class buscarCliente(TemplateView):
+
+	def post(self,request,*args,**Kwargs):
+		buscar = request.POST.get('buscalo')
+		print buscar
+		cliente = Clientes.objects.filter(cli_nombre__contains=buscar)
+		print cliente
+		return render(request,'cliente/buscarcliente.html',{'clientes':cliente,'cliente':True})
+from django.core import serializers	
+class buscadorAjaxCliente(TemplateView):
+	def get(self,request,*args,**kwargs):
+		id_cliente = request.GET.get('id')
+		print id_cliente
+		cliente = Clientes.objects.filter(id =id_cliente)
+		data = serializers.serialize('json',cliente,fields=('cli_nombre','cli_cedula','cli_direccion','cli_telefono','cli_email','cli_estado','ciu'))
+		return HttpResponse(data,content_type='application/json')
+
+class busqueda(ListView):
+	model = Clientes
+	template_name = 'cliente/busqueda.html'
+	context_object_name = 'clientes'
+
+class editar(UpdateView):
+	model = Clientes
+	template_name= 'cliente/editar.html'
+	success_url=reverse_lazy('home')
+class eliminar(DeleteView):
+	model = Clientes
+	context_object_name="clientes"
+	template_name = 'cliente/eliminar.html'
+	success_url = reverse_lazy('home')
